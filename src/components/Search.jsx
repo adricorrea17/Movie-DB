fa2c2f6113bbc8dac24b1eecf182cc67bee0cd68
adricorrea17 <adriancorrea2405@gmail.com>
2023-12-16 14:37:15 -0300

    arreglo de imgs

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,5 @@
 
 import React, { useState } from 'react';
-import axios from 'axios';
 import { SearchMovies } from '../MoviedbReposity';
 
 export default function Search() {
--- src/components/SelectLenguages.jsx
+++ src/components/SelectLenguages.jsx
@@ -8,9 +8,9 @@ export default function SelectLenguages() {
     
     return (
         <div className="flex gap-4">
-            <Link to={route(currentWihoutLanguage,'es')}><img className="w-6" src="/img/spain.png" alt="Español" /></Link>
-            <Link to={route(currentWihoutLanguage,'en')}><img className="w-6" src="/img/united-states.png" alt="English" /></Link>
-            <Link to={route(currentWihoutLanguage,'pt')}><img className="w-6" src="/img/portugal.png" alt="Português" /></Link>
+            <Link to={route(currentWihoutLanguage,'es')}><img className="w-6" src="https://adricorrea17.github.io/Movie-DB/img/spain.png" alt="Español" /></Link>
+            <Link to={route(currentWihoutLanguage,'en')}><img className="w-6" src="https://adricorrea17.github.io/Movie-DB/img/united-states.png" alt="English" /></Link>
+            <Link to={route(currentWihoutLanguage,'pt')}><img className="w-6" src="https://adricorrea17.github.io/Movie-DB/img/portugal.png" alt="Português" /></Link>
         </div>
     )
 }
